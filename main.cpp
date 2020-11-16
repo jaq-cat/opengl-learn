cb78c61e027ae3d67eebdd8ef21ca89775d6e9ad
jaq-cat <jaq.cat@protonmail.ch>
2020-11-15 21:24:17 -0300

    stuff

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <GL/glut.h>
 
-int main() {
+void display() {
+    glClearColor(1.0, 0.0, 1.0, 1.0); // #ff00ff background
+    glClear(GL_COLOR_BUFFER_BIT); // clear color buffer
+    glBegin(GL_QUADS);
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(0.5, -0.5);
+        glVertex2f(-0.5, 0.5);
+    glEnd();
+
+    glFlush(); // render
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("OpenGL!");
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(50, 50);
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
