991180b66a250ba46d3c4d8e70264015d34f127b
jaq-cat <jaq.cat@protonmail.ch>
2020-11-16 09:22:12 -0300

    kewl

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ std::string load_file(const std::string& path) {
     auto ss = std::ostringstream{};
     std::ifstream file(path);
     ss << file.rdbuf();
-    return ss.str();
+    return ss.str().c_str();
 }
 
 int main(int argc, char** argv) {
@@ -50,21 +50,20 @@ int main(int argc, char** argv) {
     glfwGetFramebufferSize(win, &width, &height);
     glViewport(0, 0, width, height);
 
-    // get version info
+    // version info
     const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
     const GLubyte* version = glGetString(GL_VERSION); // version as a string
     cout << "Renderer: " << renderer << endl;
     cout << "OpenGL version: " << version << endl;
 
-    /* 
-     * stuff
-     * */
+    // triangle
     float points[] = {
          0.0, 0.5,  0.0,
          0.5, -0.5, 0.0,
         -0.5, -0.5, 0.0
     };
 
+    // vertices or something
     GLuint vbo = 0;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -77,6 +76,7 @@ int main(int argc, char** argv) {
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
+    // shaders
     string vs_string = load_file("shaders/vertex_shader.glsl");
     string fs_string = load_file("shaders/fragment_shader.glsl");
     const char* vs_c = vs_string.c_str();
@@ -94,17 +94,20 @@ int main(int argc, char** argv) {
     glAttachShader(shader_program, vs);
     glLinkProgram(shader_program);
 
+    // main loop
     while (!glfwWindowShouldClose(win)) {
-        // wipe drawing surface
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        // use shaders
-        glUseProgram(shader_program);
-        // bind VAO
-        glBindVertexArray(vao);
+        // update
+
         // draw
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
         // poll events
         glfwPollEvents();
+
         // show stuff
         glfwSwapBuffers(win);
     }
