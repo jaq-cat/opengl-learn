dff2e5344afe7be0db8515b4790b61525c70f11c
jaq-cat <jaq.cat@protonmail.ch>
2020-11-16 08:42:46 -0300

    remove mainloop thingey

--- main.cpp
+++ main.cpp
@@ -11,12 +11,6 @@
 void display() {
     glClearColor(0.0, 0.0, 0.0, 1.0); // black opaque background
     glClear(GL_COLOR_BUFFER_BIT); // clear color buffer
-    glBegin(GL_QUADS);
-        glColor3f(1.0, 0.0, 0.0);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0.5, -0.5);
-        glVertex2f(0.5, 0.5);
-        glVertex2f(-0.5, 0.5);
     glEnd();
 
     glFlush(); // render
@@ -52,19 +46,12 @@ int main(int argc, char** argv) {
     glfwGetFramebufferSize(win, &width, &height);
     glViewport(0, 0, width, height);
 
-    glfwSwapInterval(1);
-    // main loop
-    double time;
-    while (!glfwWindowShouldClose(win)) {
-        // update
-        time = glfwGetTime();
-        // render
-        display();
-        // swap buffers
-        glfwSwapBuffers(win);
-        // poll events
-        glfwPollEvents();
-    }
+    // get version info
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION); // version as a string
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version: %s\n", version);
+
     glfwTerminate();
     return 0;
 }
