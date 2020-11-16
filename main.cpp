305262aef5d725604dde68a32b25c929de8bde8b
jaq-cat <jaq.cat@protonmail.ch>
2020-11-16 07:46:04 -0300

    squar

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ void display() {
     glClearColor(0.3, 0.0, 0.3, 1.0); // dark purple background
     glClear(GL_COLOR_BUFFER_BIT); // clear color buffer
     glBegin(GL_QUADS);
-        glColor3f(1.0, 0.0, 0.0);
+        glColor3f(1.0, 1.0, 1.0);
         glVertex2f(-0.5, -0.5);
         glVertex2f(0.5, -0.5);
+        glVertex2f(0.5, 0.5);
         glVertex2f(-0.5, 0.5);
     glEnd();
 
