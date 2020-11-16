253b525a059e2f85c1f263e5c39405cefc32601c
jaq-cat <jaq.cat@protonmail.ch>
2020-11-16 08:47:28 -0300

    stuff

--- main.cpp
+++ main.cpp
@@ -52,6 +52,27 @@ int main(int argc, char** argv) {
     printf("Renderer: %s\n", renderer);
     printf("OpenGL version: %s\n", version);
 
+    /* 
+     * stuff
+     * */
+    float points[] = {
+         0.0, 0.5,  0.0,
+         0.5, -0.5, 0.0,
+        -0.5, -0.5, 0.0
+    };
+
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+    
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
     glfwTerminate();
     return 0;
 }
