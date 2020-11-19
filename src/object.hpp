64003db30cb48c540369ab61bbccc30890b40efe
jaq-cat <jaq.cat@protonmail.ch>
2020-11-19 09:14:45 -0300

    constructor

--- src/object.hpp
+++ src/object.hpp
@@ -12,9 +12,20 @@ class Object {
         GLuint vao;
         GLuint vbo;
     public:
-        Object(vector<GLfloat> points, vector<GLfloat> colors) {
+        Object(vector<GLfloat> points, vector<GLfloat> colors, size_t i) {
             this->points = points;
             this->colors = colors;
+
+            // vao
+            glGenVertexArrays(sizeof(vao) / sizeof(GLuint), &vao);
+            glBindVertexArray(vao);
+            
+            // vbo
+            glGenBuffers(1, &vbo);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, sizeof((GLuint *) (&points[0])), &points[0], GL_STATIC_DRAW);
+            glVertexAttribPointer(i, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+            glEnableVertexAttribArray(i);
         }
 
         void draw() {
