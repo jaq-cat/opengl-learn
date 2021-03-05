a8b0933cef447c380c8e145d5d7fa9508301aeb0
jaq-cat <jaq.cat@protonmail.ch>
2021-03-05 15:49:07 -0300

    remove old code and restart tutorial

--- headers/init.hpp
+++
@@ -1,8 +0,0 @@
-#include <GL/glew.h>
-#define GLFW_DLL
-#include <GLFW/glfw3.h>
-#include "../headers/util.hpp"
-
-void makeObject(GLfloat points[], size_t points_size, GLfloat colors[], size_t colors_size, GLuint &vao);
-
-void makeShaders(GLuint &shader_program);
--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,62 @@
 #include <iostream>
 #include <string>
 #include <string_view>
-#include "../headers/util.hpp"
-#include "../headers/init.hpp"
-#include "../headers/object.hpp"
-#include "../headers/math.hpp"
 
 #include <GL/glew.h>
 #define GLFW_DLL
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+
 using std::string;
 using std::cout;
+using std::cerr;
 using std::endl;
 using std::pair;
+
+using namespace glm;
 
 #define WIDTH 640*1.8
 //#define HEIGHT 640
 #define HEIGHT 480*1.8
 
 int main(int argc, char** argv) {
-    // initialize GLFW
+    glewExperimental = GL_TRUE; // initialize GLFW
     if (!glfwInit()) {
-        fprintf(stderr, "Failed to initialize GLFW");
+        cerr <<  "Failed to initialize GLFW" << endl;
         return -1;
     }
-    GLFWwindow *win;
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // no resize/decorate
+    glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
 
-    // initialize window
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-    glfwWindowHint(GLFW_SAMPLES, 8);
-    win = glfwCreateWindow(WIDTH, HEIGHT, "Hello, OpenGL!", NULL, NULL);
-    if (!win) {
-        fprintf(stderr, "Failed to initialize GLFW window");
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+
+    GLFWwindow *win = glfwCreateWindow(WIDTH, HEIGHT, "Hello, OpenGL!", NULL, NULL); // initialize window
+    if (win == NULL) {
+        cerr << "Failed to initialize GLFW window" << endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(win);
 
     // GLEW
-    glewExperimental = GL_TRUE;
-    glewInit();
-
-    // configuration
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_MULTISAMPLE);
-    glDepthFunc(GL_LESS);
-    glLineWidth(1);
+    if (glewInit() != GLEW_OK) {
+        cerr << "Failed to initialzie GLEW" << endl;
+        glfwTerminate();
+        return -1;
+    }
 
     // print opengl info
     cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
-    cout << "OpenGL version: " << glGetString(GL_VERSION) << endl;
     cout << "OpenGL vendor: " << glGetString(GL_VENDOR) << endl;
+    cout << "OpenGL version: " << glGetString(GL_VERSION) << endl;
 
-    // shape A
-    GLfloat a_points[] = {
-        -0.3,  0.8,  0.0, // top left
-         0.3,  0.8,  0.0, // top right
-        -0.6, -0.1, 0.0, // bottom left
-         0.6, -0.1, 0.0, // bottom right
-         0.0, -0.9, 0.0, // bottom
-    };
-    GLfloat a_colors[] = {
-        1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, // RGB instead of XYZ
-        0.0, 1.0, 1.0,
-        0.0, 1.0, 1.0,
-        0.0, 0.0, 1.0
-    };
-    Object obj_a(a_points, sizeof(a_points), a_colors, sizeof(a_colors));
+    glfwSetInputMode(win, GLFW_STICKY_KEYS, GL_TRUE);
 
-    // obj B
-    GLfloat b_points[] = {
-        -0.5,  1.5, 0.0, // top left
-         0.5,  1.5, 0.0, // top right
-        -0.5,  1.0, 0.0, // bottom right
-         0.5,  1.0, 0.0, // bottom left
-    };
-    GLfloat b_colors[] = {
-        1.0, 0.0, 0.0,
-        1.0, 1.0, 0.0,
-        1.0, 0.0, 0.0,
-        1.0, 1.0, 0.0,
-    };
-    Object obj_b(b_points, sizeof(b_points), b_colors, sizeof(b_colors));
-
-    GLuint shader_program;
-    makeShaders(shader_program);
     // main loop
     while (!glfwWindowShouldClose(win)) {
         // update
@@ -96,22 +65,15 @@ int main(int argc, char** argv) {
         glClearColor(0.0, 0.05, 0.1, 1);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // draw points
-        glUseProgram(shader_program);
-        obj_a.draw();
-        obj_b.draw();
-
-        // floosh
         glFlush();
-
-        // poll events
         glfwPollEvents();
 
         // show stuff
         glfwSwapBuffers(win);
-        if (glfwGetKey(win, GLFW_KEY_ESCAPE)) {
+
+        // close window
+        if (glfwGetKey(win, GLFW_KEY_ESCAPE))
             glfwSetWindowShouldClose(win, 1);
-        }
     }
 
     glfwTerminate();
