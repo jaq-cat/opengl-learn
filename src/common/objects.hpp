357dcdedaa506c2a575a8ea9a00888dbbabf7925
jaq-cat <jaq.cat@protonmail.ch>
2021-06-03 14:34:50 -0300

    restart

--- src/common/objects.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <GL/glew.h>
-
-GLuint createObject(const GLfloat vb_data[], const int vb_data_sizeof);
-void drawObject(const GLfloat objSize, const GLuint objId, const GLuint matrixId, const GLfloat *objMvp);
-void useColors(const GLuint colorId);
