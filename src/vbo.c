836c004ab44db085ec91b32ed447d907c1753e02
jaq-cat <jaq.cat@protonmail.ch>
2021-06-15 09:29:59 -0300

    hmmm

--- src/vbo.c
+++ src/vbo.c
@@ -5,7 +5,7 @@ int create(GLfloat data[], int size) {
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
-    /*glBindBuffer(GL_ARRAY_BUFFER, 0);*/
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     return vbo;
 }
